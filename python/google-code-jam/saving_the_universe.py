09b30dca26feb02faf0bbf46319785e307684d26
thisiswei <99plus2@gmail.com>
2013-06-09 19:36:42 -0400

    google-code-jam-2008-problem a

---
+++ python/google-code-jam/saving_the_universe.py
@@ -0,0 +1,34 @@
+
+if __name__ == '__main__':
+    N = int(raw_input())
+    for test in range(N):
+        engines, queries= [], []
+        [engines.append(raw_input()) for i in int(raw_input())]
+        [queries.append(raw_input()) for q in int(raw_input())]
+        if len(engines) > set(queries): 
+            print 'Case #%d: %d' % (test+1, 0)
+            continue
+        explored = set()
+        res = 0
+        L = len(engines)
+        for q in queries: 
+            explored.add(q)
+            if len(explored) == L:
+                res += 1 
+                explored = set([q])
+        print 'Case #%d: %d' % (test+1, res)
+
+                   
+
+
+"""Input 
+
+Output 
+2 5 Yeehaw NSM Dont Ask B9 Googol
+10 Yeehaw Yeehaw Googol B9 Googol NSM B9 NSM Dont Ask Googol
+5 Yeehaw NSM Dont Ask B9 Googol
+7 Googol Dont Ask NSM NSM Yeehaw Yeehaw Googol 
+Case #1: 1
+Case #2: 0
+
+"""
